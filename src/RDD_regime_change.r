d58203a538e3e2664830186cd750623318fd2d2d
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-22 13:24:38 +0200

    RDD script trying to work

--- src/RDD_regime_change.r
+++ src/RDD_regime_change.r
@@ -5,7 +5,6 @@
 
 
 setwd("/home/dominik/Documents/seminar_econo/econo_seminar/")
-# Table Reading
 EXP_PATH = "img/"
 IMP_PATH = "data/"
 DAT_PATH = "regime_change_5_years.csv"
@@ -28,4 +27,21 @@ treatment_group_years = subset(dataframe$year,
                              dataframe$regime.change.within.5.years == 1)
 
 plot(treatment_group_years, treatment_group,
-     type="n")
+     type="p",
+     pch=0,
+     cex=0.1,
+     xlab="Year",
+     ylab="Control group",
+     col="black")
+
+points(control_group_years, control_group,
+       col="red",
+       pch=0,
+       cex=0.1)
+with(subset(dataframe$conflict, 
+             dataframe$regime.change.within.5.years == 2),
+      lines(col="red"))
+with(subset(dataframe$conflict,
+            dataframe$regime.change.within.5.years == 1),
+     lines(col="blue"))
+help(with)
