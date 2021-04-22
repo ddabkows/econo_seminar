3a216ab57b9ca0852dcadefdbfc4e7ddca17f611
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-22 13:24:29 +0200

    Changed of target folder

--- src/gdp_dist_calc.py
+++ src/gdp_dist_calc.py
@@ -43,7 +43,7 @@ def main():
                         database["dist_avg_gcp_ppc"][row_to_check] = distance
 
             print(str(round(float(row_to_check/size)*100, 2)) + " % done.")
-        database.to_csv("/home/dominik/Documents/seminar_econo/econo_seminar/dist_to_biggest_gdps.csv")
+        database.to_csv("/home/dominik/Documents/seminar_econo/econo_seminar/data/dist_to_biggest_gdps.csv")
 
 
 if __name__ == "__main__":
