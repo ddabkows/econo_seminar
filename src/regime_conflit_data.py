aaa6f09d3012745c3935a9d45f93feb1082b2520
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-24 15:20:00 +0200

    Added logdistcap to the variables

--- src/regime_conflit_data.py
+++ src/regime_conflit_data.py
@@ -6,6 +6,7 @@ Code:   Adding last regime change year variable to the workingfile data to know
 """
 
 import pandas
+import time
 
 FIRST_ITERATION: int = 6
 
@@ -13,7 +14,7 @@ FIRST_ITERATION: int = 6
 def main():
     with open("../data/regime_change_5_years.csv") as dataset:
         database = pandas.read_csv(dataset,
-                                   usecols=["id", "gid", "year", "conflict", "regime change"],
+                                   usecols=["id", "gid", "year", "conflict", "regime change", "logdistcap"],
                                    sep=',')
         print(database.head())
         size: int = len(database["year"])
@@ -30,9 +31,12 @@ def main():
                             if database["regime change"][row_check] == 1:
                                 regime_change_5_years = 1
                 print("Recording " + str(regime_change_5_years) + " to " + str(database["gid"][row_starting_points]) +
-                      ".")
+                      " in " + str(database["year"][row_starting_points]) + ".")
                 database["regime change within 5 years"][row_starting_points] = regime_change_5_years
-        database.to_csv("/home/dominik/Documents/seminar_econo/econo_seminar/regime_change_5_years.csv")
+            if (row_starting_points / size * 100) % 2 == 0:
+                print(" " * 10 + str(round(row_starting_points / size * 100, 2)) + " % of the data covered.")
+                time.sleep(1)
+        database.to_csv("/home/dominik/Documents/seminar_econo/econo_seminar/data/regime_change_5_years.csv")
 
 
 if __name__ == "__main__":
