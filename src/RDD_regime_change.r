9b54dff5ba8ffe5814b09de88b75075b76dd6503
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-25 17:26:14 +0200

    Fixed effects linear regression

--- src/RDD_regime_change.r
+++ src/RDD_regime_change.r
@@ -4,10 +4,11 @@
 #          regime change
 
 
+library(plm)
 library(aod)
-library(rr2)
-
-
+library(bife)
+install.packages('bife')
+install.packages('RcppArmadillo')
 
 
 setwd("/home/dominik/Documents/seminar_econo/econo_seminar/")
@@ -21,50 +22,45 @@ dataframe = read.csv(paste(IMP_PATH,
                            sep=""),
                      stringsAsFactors=T,
                      sep=",")
+dataframe = pdata.frame(dataframe, index=c("year"))
+regression = conflict ~ logdistcap + loggcppc + logpop + imr +
+                        logttime + logcellarea + logdist_LNC + 
+                        mountain2000 + ycoord + degtemper + prec
 
 # Control group subset 
-control_group = subset(dataframe$conflict, 
+control_group = subset(dataframe, 
                        dataframe$regime.change.within.5.years == 2)
-control_group_logdistcap = subset(dataframe$logdistcap,
-                             dataframe$regime.change.within.5.years == 2)
-
-
-control_proportion = length(control_group) / length(dataframe$conflict) * 100
+control_proportion = length(control_group$conflict) / 
+                     length(dataframe$conflict) * 100
 
 # Treatment group subset
-treatment_group = subset(dataframe$conflict,
+treatment_group = subset(dataframe,
                          dataframe$regime.change.within.5.years == 1)
-treatment_group_logdistcap = subset(dataframe$logdistcap,
-                             dataframe$regime.change.within.5.years == 1)
-
-treatment_proportion = length(treatment_group) / length(dataframe$conflict)* 100
+treatment_proportion = length(treatment_group$conflict) / 
+                       length(dataframe$conflict)* 100
 
 # No elligible group subset
-noneli_group = subset(dataframe$conflict,
+noneli_group = subset(dataframe,
                       dataframe$regime.change.within.5.years == 0 & 
                               dataframe$regime.change == 0)
-noneli_group_logdistcap = subset(dataframe$logdistcap,
-                            dataframe$regime.change.within.5.years == 0 & 
-                                    dataframe$regime.change == 0)
+noneli_proportion = length(noneli_group$conflict) / 
+                    length(dataframe$conflict) * 100
 
-noneli_proportion = length(noneli_group) / length(dataframe$conflict) * 100
 
-treatment_reg = glm(treatment_group ~ treatment_group_logdistcap, 
-                    family="binomial")
+treatment_reg = plm(regression,
+                    data = treatment_group,
+                    model="within")
 treatment_pred = predict(treatment_reg)
 summary(treatment_reg)
 
-control_reg = glm(control_group ~ control_group_logdistcap, 
-                  family="binomial")
+control_reg = plm(regression,
+                  data=control_group,
+                  model="within")
 control_pred = predict(control_reg)
 summary(control_reg)
 
-noneli_reg = glm(noneli_group ~ noneli_group_logdistcap,
-                 family="binomial")
+noneli_reg = plm(regression,
+                 data=noneli_group,
+                 model="within")
 noneli_pred = predict(noneli_reg)
 summary(noneli_reg)
-
-
-plot(dataframe$logdistcap, dataframe$conflict)
-lmreg = glm(conflict ~ logdistcap, data=dataframe, family="binomial")
-summary(lmreg)
