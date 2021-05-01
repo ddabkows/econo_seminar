50db289062c48015ecfdf7e21b928b0ead05cf27
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-05-01 15:09:39 +0200

    Huge misunderstanding of RDD solved

--- src/regime_conflit_data.py
+++ src/regime_conflit_data.py
@@ -7,9 +7,12 @@ Code:   Adding last regime change year variable to the workingfile data to know
 
 
 import pandas
+import numpy
 import time
 
 FIRST_ITERATION: int = 6
+FIRST_DATE: int = 1946
+LAST_DATE: int = 2005
 
 
 def main():
@@ -20,21 +23,32 @@ def main():
                                    sep=',')
         print(database.head())
         size: int = len(database["year"])
-        database["transition6"] = 0
-        for row_starting_points in range(FIRST_ITERATION, size):
-            regime_change_5_years: int = 0
-            for row_check in range(row_starting_points - FIRST_ITERATION, row_starting_points):
+        database["last_transition"] = numpy.nan
+        database["next_transition"] = numpy.nan
+        database["pre_transition"] = numpy.nan
+        for row_starting_points in range(0, size):
+            last_transition: float = numpy.nan
+            next_transition: float = numpy.nan
+            for row_check in range(row_starting_points-1, -1, -1):
                 if database["gid"][row_starting_points] == database["gid"][row_check]:
-                    if database["year"][row_starting_points] - database["year"][row_check] == 6:
-                        if database["transition"][row_check] == 1:
-                            regime_change_5_years = 2
-                    if database["year"][row_starting_points] - database["year"][row_check] <= 5:
-                        if database["transition"][row_check] == 1:
-                            regime_change_5_years = 1
-            print("Recording " + str(regime_change_5_years) + " to " + str(database["gid"][row_starting_points]) +
-                  ", " + str(database["isocode"][row_starting_points]) + " in " +
-                  str(database["year"][row_starting_points]) + ".")
-            database["transition6"][row_starting_points] = regime_change_5_years
+                    if database["transition"][row_check] == 1:
+                        last_transition = database["year"][row_starting_points] - database["year"][row_check]
+                        break
+                elif database["gid"][row_starting_points] != database["gid"][row_check]:
+                    break
+            for row_check in range(row_starting_points+1, size):
+                if database["gid"][row_starting_points] == database["gid"][row_check]:
+                    if database["transition"][row_check] == 1:
+                        next_transition = database["year"][row_check] - database["year"][row_starting_points]
+                elif database["gid"][row_starting_points] != database["gid"][row_check]:
+                    break
+            if (next_transition < last_transition) and (next_transition <= 5):
+                database["pre_transition"][row_starting_points] = 1
+            elif (last_transition < next_transition) and (last_transition <= 5):
+                database["pre_transition"][row_starting_points] = 0
+            database["last_transition"][row_starting_points] = last_transition
+            database["next_transition"][row_starting_points] = next_transition
+
             if (row_starting_points / size * 100) % 2 == 0:
                 print(" " * 10 + str(round(row_starting_points / size * 100, 2)) + " % of the data covered.")
                 time.sleep(1)
