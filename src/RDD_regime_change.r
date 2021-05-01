b833fc7319b34378cdd9942e04ea540066a2e8af
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-05-01 17:02:23 +0200

    Regressions for the RDD

--- src/RDD_regime_change.r
+++ src/RDD_regime_change.r
@@ -23,53 +23,29 @@ dataframe = read.csv(paste(IMP_PATH,
 dataframe = pdata.frame(dataframe, index=c("year"))
 regression = ConfIntra ~ logcapdist + logbdist2 + 
              degtemper + prec | year 
-trans_regression = ConfIntra ~ transition6 + logcapdist + logbdist2 + 
-  degtemper + prec | year 
 
 # Control group subset 
-control_group = subset(dataframe, 
-                       dataframe$transition6 == 2)
-control_proportion = length(control_group$ConfIntra) / 
-                     length(dataframe$ConfIntra) * 100
-plot(control_group$logcapdist, control_group$ConfIntra)
+pre_test_group = subset(dataframe,
+                        dataframe$pre_transition == 1)
+pre_test_prop  = length(pre_test_group$ConfIntra) /
+                 length(dataframe$ConfIntra) * 100
 
-# Treatment group subset
-treatment_group = subset(dataframe,
-                         dataframe$transition6 == 1)
-treatment_proportion = length(treatment_group$ConfIntra) / 
-                       length(dataframe$ConfIntra)* 100
-plot(treatment_group$logcapdist, treatment_group$ConfIntra)
-
-# No elligible group subset
-noneli_group = subset(dataframe,
-                      dataframe$transition6 == 0)
-noneli_proportion = length(noneli_group$ConfIntra) / 
-                    length(dataframe$ConfIntra) * 100
-plot(noneli_group$logcapdist, noneli_group$ConfIntra)
+post_test_group = subset(dataframe,
+                        dataframe$pre_transition == 0)
+post_test_prop  = length(post_test_group$ConfIntra) /
+  length(dataframe$ConfIntra) * 100
 
 
-treatment_reg = bife(regression,
-                    data = treatment_group,
+pre_test_reg = bife(regression,
+                    data = pre_test_group,
                     model="logit")
-treatment_pred = predict(treatment_reg)
-summary(treatment_reg)
+pre_test_pred = predict(pre_test_reg)
+summary(pre_test_reg)
 
-control_reg = bife(regression,
-                  data=control_group,
-                  model="logit")
-control_pred = predict(control_reg)
-summary(control_reg)
-AIC(control_reg)
+post_test_reg = bife(regression,
+                     data = post_test_group,
+                     model="logit")
+post_test_pred = predict(post_test_reg)
+summary(post_test_reg)
 
-noneli_reg = bife(regression,
-                 data=noneli_group,
-                 model="logit")
-noneli_pred = predict(noneli_reg)
-summary(noneli_reg)
-
-trans6_reg = bife(trans_regression, 
-                  data = dataframe,
-                  model='logit')
-trans6_pred = predict(trans6_reg)
-summary(trans6_reg)
 
